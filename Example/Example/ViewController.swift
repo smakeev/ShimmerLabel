309211738ccc451005c3c918deef2dbb1835232e
Sergey Makeev <makeev.87@gmail.com>
2020-03-22 15:20:46 +0300

    initial example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Sergey Makeev on 22.03.2020.
+//  Copyright © 2020 SOME projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
