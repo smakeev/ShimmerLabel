e53cc0667de35d0be17245f76063491045939d46
Sergey Makeev <makeev.87@gmail.com>
2020-03-22 14:42:48 +0300

    initial code

---
+++ ShimmerLabel/ShimmerLabel/ShimmerLabel.h
@@ -0,0 +1,19 @@
+//
+//  ShimmerLabel.h
+//  ShimmerLabel
+//
+//  Created by Sergey Makeev on 22.03.2020.
+//  Copyright © 2020 SOME projects. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShimmerLabel.
+FOUNDATION_EXPORT double ShimmerLabelVersionNumber;
+
+//! Project version string for ShimmerLabel.
+FOUNDATION_EXPORT const unsigned char ShimmerLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShimmerLabel/PublicHeader.h>
+
+
